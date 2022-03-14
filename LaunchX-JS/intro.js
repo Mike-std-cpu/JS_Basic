edffe86974b32473ca3144951104752ebfd8ce89
Mike&rade <hoodloom07@outlook.es>
2022-03-13 18:20:31 -0600

    Intro de javaScript

--- LaunchX-JS/intro.js
+++ LaunchX-JS/intro.js
@@ -148,3 +148,59 @@ const restar = (a,b) => {
     return a - b;
 }
 console.log(restar(5,1));
+
+
+console.log("\n********** excepciones ***********\n");
+function preguntaDireccion(pregunta) {
+    let result = prompt(pregunta);
+    if (result.toLowerCase() == "izquierda") return "I";
+    if (result.toLowerCase() == "derecha") return "D";
+    throw new Error("Dirección inválida: " + result);
+  }
+  
+  function mirar() {
+    if (preguntaDireccion("A que lado?") == "I") {
+      return "una casa";
+    } else {
+      return "2 osos hambrientos";
+    }
+  }
+  
+  try {
+    console.log("Mira a ", mirar());
+  } catch (error) {
+    console.log("Hubo un error: " + error);
+  }
+  
+  
+  
+  //Asincrono
+  setTimeout(() => console.log("Tick"), 500);
+  
+  let fifteen = Promise.resolve(15);
+  fifteen.then(value => console.log(`Got ${value}`));
+  
+  const promesa = () =>
+    new Promise((resolve, reject) =>
+      setTimeout(
+        () => (resolve(console.log('Todo cool')), reject(new Error('oops'))),
+        2000
+      )
+    )
+  
+  async function main() {
+    //   promesa()
+    //     .then(() => {
+    //       promesa()
+    //         .then(() => console.log('hola'))
+    //         .catch((err) => console.error(err))
+    //     })
+    //     .catch((err) => console.error(err))
+  
+    await promesa();
+    console.log('Aquí termina la primer promesa');
+    await promesa();
+    console.log('Aquí termina la segunda promesa');
+  }
+  
+  main();
