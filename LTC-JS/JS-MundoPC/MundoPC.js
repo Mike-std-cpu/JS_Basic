38700684b891b2331d63bbf7fb1be035ead4523c
Mike&rade <hoodloom07@outlook.es>
2022-04-12 02:51:21 -0500

    Changes in MundoPC and comments

--- LTC-JS/JS-MundoPC/MundoPC.js
+++ LTC-JS/JS-MundoPC/MundoPC.js
@@ -1,17 +1,17 @@
-class DispoitivoEntrada{
-    constructor(entrada, marca){
+class DispoitivoEntrada{ //Clase de dispositivos de entrada
+    constructor(entrada, marca){ // Constructor para obtener los atributos
         this._entrada = entrada;
         this._marca = marca;
     }
 
-    get entrada(){
+    get entrada(){ // Para mostrar
         return this._entrada;
     }
     get marca(){
         return this._marca;
     }
 
-    set entrada(entrada){
+    set entrada(entrada){ // Para modificar
         this._entrada = entrada; // Se insertara el parametro a this. que es del atributo de la clase
     }
     set marca(marca){
@@ -19,12 +19,12 @@ class DispoitivoEntrada{
     }
 }
 
-class Raton extends DispoitivoEntrada{
-    static contadorRatones = 0;
+class Raton extends DispoitivoEntrada{ // Raton heredara las caracteristicas de DE
+    static contadorRatones = 0; // Para poder ver la cantidad de instancias
 
     constructor(entrada, marca){
-        super(entrada,marca);
-        this._idRaton = ++Raton.contadorRatones;
+        super(entrada,marca);//Los atributos que se heredaran
+        this._idRaton = ++Raton.contadorRatones; // Atributos nuevos
     }
     get idRaton(){
         return this._idRaton;
@@ -38,4 +38,6 @@ class Raton extends DispoitivoEntrada{
 }
 
 let raton1 = new Raton("USB","HP")
-console.log(raton1.toString());
+console.log(raton1.toString());// podemos ver la primera instancia
+let raton2 = new Raton("Bluetooth", "DELL");
+console.log(raton2.toString()); // ID se incrementea a 2-->
