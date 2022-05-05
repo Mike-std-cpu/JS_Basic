d7f28959d2492b27218a7140e9dfb190010b9b10
Mike&rade <hoodloom07@outlook.es>
2022-05-04 23:38:48 -0500

    Metodo Async de promesas

---
+++ L19-JS/19-02-Async.js
@@ -0,0 +1,16 @@
+let promesa =  new Promise((resolver) => {
+    setTimeout(() => {
+        resolver('Saludos con promesa TimeOut'), 3000 ;
+    })
+})
+
+async function miFuncionConPromesa(){
+   return 'Saludos con ASYNC';
+}
+
+miFuncionConPromesa().then(valor => console.log(valor));
+
+/**
+ * Lo que podemos ver es que con asyn valoramos como una promesa que tan solo mandarla a llamar con then, podemos
+ * sacar el valor de la promesa, en este aso se agregan los (a la funcion por que es una funcion)
+ */
