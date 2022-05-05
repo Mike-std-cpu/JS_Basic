f8f6440cece5766a7b21f6dada375ab4f7f6dc8a
Mike&rade <hoodloom07@outlook.es>
2022-05-04 23:28:57 -0500

    Promesas con TimeOut

--- L19-JS/19-01-Promesas.js
+++ L19-JS/19-01-Promesas.js
@@ -7,6 +7,18 @@ let promesa = new Promise((resolver, rechazar) => {
     }
 });
 
-promesa
-.then(valor => console.log(valor))
-.catch(error => console.log(error));
+// Se crea un objeto tipo promise que tendra una funcion que tendra como parametros 'resolver' y 'rechazar' se crea la condiciòn
+// - se podra llamar declrando el objeto dentro de la variable promesa y de ahi con .then y. catch se regresara en caso de exito o rechazo.
+
+promesa.then(valor => console.log(valor)).catch(error => console.log(error));
+
+// Ejemplo con setTimeOut
+
+let promesa2 = new Promise((resolver) => {
+    setTimeout(() => {
+        resolver('Saludos con promesa y TimeOut'), 5000
+        console.log('Adios..')
+    })
+});
+
+promesa2.then(resolver => console.log(resolver));
