0a6be7cb50ef15fe65bbdecd3691671af106ee86
Mike&rade <hoodloom07@outlook.es>
2022-05-14 00:16:41 -0500

    Mas operaciones agregadas

--- LTC-JS/JS-Calculadora/App/app.js
+++ LTC-JS/JS-Calculadora/App/app.js
@@ -1,13 +1,48 @@
 console.log('Estoy en app.js 🌚');
 
-function sumar(){
+function sumar() {
     const forma = document.getElementById('forma');
     let operandoA = forma['operandoA'];
     let operandoB = forma['operandoB'];
     let resultado = parseInt(operandoA.value) + parseInt(operandoB.value);
-    if(isNaN(resultado)){
+    if (isNaN(resultado)) {
         resultado = 'La operacion no puede ejecutarse por que no hay números.'
     }
-    document.getElementById('Resultado').innerHTML = `El resultado es: ${resultado}`
+    document.getElementById('Resultado').innerHTML = `El resultado es: ${resultado}`;
     console.log('Se acaba el calculo')
+}
+function restar() {
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) -  parseInt(operandoB.value);
+    if (isNaN(resultado)) {
+        resultado = 'La operacion no puede ejecutarse por que no hay números.'
+    }
+    document.getElementById('Resultado').innerHTML = `El resultado es: ${resultado}`;
+    console.log('Se acaba el calculo')
+}
+
+function dividir() {
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) /  parseInt(operandoB.value);
+    if (isNaN(resultado)) {
+        resultado = 'La operacion no puede ejecutarse por que no hay números.'
+    }
+    document.getElementById('Resultado').innerHTML = `El resultado es: ${resultado}`;
+    console.log('Se acaba el calculo');
+}
+
+function multipli() {
+    const forma = document.getElementById('forma');
+    let operandoA = forma['operandoA'];
+    let operandoB = forma['operandoB'];
+    let resultado = parseInt(operandoA.value) *  parseInt(operandoB.value);
+    if (isNaN(resultado)) {
+        resultado = 'La operacion no puede ejecutarse por que no hay números.'
+    }
+    document.getElementById('Resultado').innerHTML = `El resultado es: ${resultado}`;
+    console.log('Se acaba el calculo');
 }
