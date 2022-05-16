630a0e843cc426f5ff41d781b03e22fc942426aa
Mike&rade <hoodloom07@outlook.es>
2022-05-16 00:34:43 -0500

    Agregacion de nombres y apellidos desde un input externo

--- LTC-JS/JS-ListadoPersonas/app/app.js
+++ LTC-JS/JS-ListadoPersonas/app/app.js
@@ -14,3 +14,20 @@ function showPersonas(){
     }
     document.getElementById('personas').innerHTML = text;
 }
+
+function agregarPerson(){
+    const forma = document.forms['forma']; // Recuperamos los objetos del forms
+    const name = forma['name'];
+    const lastName = forma['lastName'];
+    if(name.value != '' && lastName != ''){
+        const persona = new Persona(name.value, lastName.value);
+        console.log(persona)
+        Personas.push(persona);
+        showPersonas();
+    }else{
+        alert("Campos Vacios, Agregue un nombre y un apellido.👻")
+    }
+
+
+
+}
