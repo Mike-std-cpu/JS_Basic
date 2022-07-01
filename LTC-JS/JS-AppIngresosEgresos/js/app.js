bd94d138d5337c3fd406704f8b37b9856a3bf977
Mike&rade <hoodloom07@outlook.es>
2022-07-01 03:41:18 -0500

    Arreglos de ingresos y egresos añadidos

---
+++ LTC-JS/JS-AppIngresosEgresos/js/app.js
@@ -0,0 +1,16 @@
+const ingresos  =[
+    new Ingreso('Salario', 2100),
+    new Ingreso('Venta coche', 1500)
+];
+
+const egresos = [
+    new Egreso('Renta de departamento', 900),
+    new Egreso('Ropa',400)
+];
+
+let cargarapp = () =>{
+    cargarCabecero();
+}
+let cargarCabecero = () =>{
+
+}
