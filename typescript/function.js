c5c96db3ea2cbff779b29aadd93bb8c81053dc58
Mike&rade <hoodloom07@outlook.es>
2022-07-05 19:27:08 -0500

    Variables y info de TypeScript

--- typescript/function.js
+++ typescript/function.js
@@ -2,5 +2,20 @@
 function hello() {
     console.log("¡Hello there!");
     console.log("I'm here 🙂");
+    console.log("More string");
+    // Variables
+    //String
+    let cadena = "Soy un string";
+    // Number
+    let numero = 10;
+    // let numero:number = 100;
+    //any
+    let cualquiera;
+    cualquiera = "Cadena soy";
+    cualquiera = 10;
+    // constantes
+    const PI = 3.1416;
+    //PI = 1569.25 no se puede modifiica ya que es una constante.
+    console.log(`String: ${cadena} & Number: ${numero}`);
 }
 hello();
--- typescript/tsInfo.md
+++ typescript/tsInfo.md
@@ -12,3 +12,9 @@ tsc function.ts
 ````
 
 in this moment the terrminal run the script and automathic it create a new script of .js.
+
+## Variables.
+
+In typeScript we use reservation word like javaScript, but in this case in TypeScript we use `let` no `var`
+
+The constants are similiar to JavaScript, it's same.
