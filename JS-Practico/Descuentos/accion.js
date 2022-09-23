2b932067365ab324d86f0f77f8cc2380e35be8f0
Mike Andrade <hoodloom07@outlook.es>
2022-09-23 18:08:24 -0500

    Calculadora (sin estilos) finalizado

--- JS-Practico/Descuentos/accion.js
+++ JS-Practico/Descuentos/accion.js
@@ -5,5 +5,18 @@ function calcularPrecioDescuento(precioInicial, descuento){
 
     return precioFinalDescuento;
 }
-
 console.log(calcularPrecioDescuento(300,30));
+
+function PriceDiscount(){
+    const inputPrice = document.getElementById("price");
+    const valuePrice = inputPrice.value;
+    const inputDiscount = document.getElementById("discount");
+    const valueDiscount = inputDiscount.value;
+
+    //calculos
+
+    const precioFinal = calcularPrecioDescuento(valuePrice,valueDiscount);
+
+    const mostrar = document.getElementById("showPrice");
+    mostrar.innerText = `El descuento es de: ${precioFinal}`
+}
